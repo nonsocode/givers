5e93c6977e6503d1dfde6ef74da3fdd0881ad875
Nonso Chukwuogor <nonso4yoo@gmail.com>
2017-04-19 09:59:19 +0100

    Created artisan command for Clearing database

---
+++ app/Console/Commands/RefreshDatabase.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class RefreshDatabase extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'db:clean';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Clear all tables from the database';
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        \DB::raw('DROP DATABASE '.env('DB_DATABASE'));
+        \DB::raw('CREATE DATABASE '.env('DB_DATABASE'));
+    }
+}
