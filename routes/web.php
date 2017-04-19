1760b17ab3f0c47caeec1ac3a4ebad5a3bba8c62
Nonso Chukwuogor <nonso4yoo@gmail.com>
2017-04-19 10:05:33 +0100

    Fixed authentication route issues

--- routes/web.php
+++ routes/web.php
@@ -12,26 +12,30 @@
 */
 
 // Route::group(['domain' => 'givers.app'], function() {
-	Auth::routes();
+Auth::routes();
+Route::get('user/activation/{token}', 'Auth\RegisterController@activateUser')->name('user.activate');
+Route::get('user/activation/resend/token', 'Auth\RegisterController@resendForm')->name('user.activate.resend');
+Route::post('user/activation/resend', 'Auth\RegisterController@resendToken')->name('resendToken');
 
-	Route::get('/', 'PagesController@index');
-	Route::get('/home', 'PagesController@index')->middleware(['auth']);
 
-	Route::group(['prefix' => 'dashboard', 'middleware' => ['auth']], function() {
-		Route::get('/', 'OfficeController@index')->name('dashboard');
-		Route::get('refferals', 'OfficeController@refferals');
+Route::get('/', 'PagesController@index');
+Route::get('/home', 'PagesController@index')->middleware(['auth']);
 
-		Route::get('tickets', function (){return view('office.tickets.index');});
-		Route::get('support-tickets', 'TicketsController@index')->name('tickets');
-		Route::get('support-tickets/create', 'TicketsController@create')->name('tickets.create');
-		Route::post('support-tickets/store', 'TicketsController@store')->name('tickets.store');
-		Route::get('support-tickets/{ticket}', 'TicketsController@show')->name('ticket.view');
-		Route::patch('support-tickets/{ticket}', 'TicketsController@closeTicket')->name('ticket.close');
-		Route::post('support-tickets/{ticket}/new-message', 'TicketsController@newTicketMessage')->name('newTicketMessage');
+Route::group(['prefix' => 'dashboard', 'middleware' => ['auth']], function() {
+	Route::get('/', 'OfficeController@index')->name('dashboard');
+	Route::get('refferals', 'OfficeController@refferals');
 
-		Route::get('profile', "ProfileController@index")->name('profile');
-		Route::get('bonuses', "BonusController@index")->name('bonuses.index');
-	});
+	Route::get('tickets', function (){return view('office.tickets.index');});
+	Route::get('support-tickets', 'TicketsController@index')->name('tickets');
+	Route::get('support-tickets/create', 'TicketsController@create')->name('tickets.create');
+	Route::post('support-tickets/store', 'TicketsController@store')->name('tickets.store');
+	Route::get('support-tickets/{ticket}', 'TicketsController@show')->name('ticket.view');
+	Route::patch('support-tickets/{ticket}', 'TicketsController@closeTicket')->name('ticket.close');
+	Route::post('support-tickets/{ticket}/new-message', 'TicketsController@newTicketMessage')->name('newTicketMessage');
+
+	Route::get('profile', "ProfileController@index")->name('profile');
+	Route::get('bonuses', "BonusController@index")->name('bonuses.index');
+});
 // });
 Route::group(['prefix'=>'json','middleware' =>['auth']],function(){
 	Route::resource('phs','JSON\ProvideHelpController');
