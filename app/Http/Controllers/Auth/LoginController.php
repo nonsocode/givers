91fa8fccf5465a9f958969373954c3ddebe1e207
Nonso Chukwuogor <nonso4yoo@gmail.com>
2017-05-10 21:37:42 +0100

    login redirect fixed

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/dashboard2';
+    protected $redirectTo = '/dashboard';
 
     /**
      * Create a new controller instance.
